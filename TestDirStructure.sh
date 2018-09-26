4f827845a05ec6f99d01c812109bc459a6ca800e
EC2 Default User <ec2-user@ip-172-31-26-171.ec2.internal>
2018-09-26 16:51:48 +0000

    Added Test script and v1.2.1

---
+++ TestDirStructure.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+Replace = /home/ec2-user/Replace
+Source = /home/ec2-user/Replace/tmp
+Etc = /home/ec2-user/Replace/etc
+Staging = /home/ec2-user/Replace/Staging
+
+if[ -d "$Replace" ]; 
+then
+ echo "Replace Dir exists"
+ rm -rf $Replace
+fi
+
+mkdir $Replace
+mkdir $Source
+mkdir $Staging
+mkdir $Etc
+
+echo "Modded Yum" > "$Source/yum.conf"
+echo "Modded Repo" > "$Source/yum.repos.d/amzn-updates.repo"
+echo "Orignal Yum" > "$Etc/yum.conf"
+echo "Original Repo" > "$Etc/yum.repos.d/amzn-updates.repo"
