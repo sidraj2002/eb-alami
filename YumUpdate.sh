2b1ab7a1c6b761a4f21d2ea735a5a1019b81376c
EC2 Default User <ec2-user@ip-172-31-26-171.ec2.internal>
2018-09-26 20:15:59 +0000

    Added separate revert function and variable to run Yum commands

--- YumUpdate.sh
+++ YumUpdate.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -xe
+
+    #Script currently replaces original file with modded file, then based on YUM_NO_REVERT variable will either.. 
+    #revert back to original or keep modded config. Default = revert).
+
     #FilesNames#
     _YumConfFile='yum.conf'
     _YumRepoFile='amzn-updates.repo'
 
     #SourceFilePaths#
-    _YumRepoFilePath='/home/ec2-user/Replace/tmp/'
+    _YumRepoFilePath='/home/ec2-user/Replace/tmp/yum.repos.d/'
     _YumConfFilePath='/home/ec2-user/Replace/tmp/'
     #_DefaultYumConfFile='/etc/yum.conf'
     #_DefaultYumRepoFile='/etc/yum.repos.d/amzn-updates.repo'
@@ -19,7 +23,9 @@ set -xe
     _StagingYumRepoFilePath='/home/ec2-user/Replace/Staging/'
 
     #Revert Setting
-    _YUM_DONT_REVERT=$(/opt/elasticbeanstalk/bin/get-config environment -k YUM_DONT_REVERT);
+    _YUM_DONT_REVERT=$(/opt/elasticbeanstalk/bin/get-config environment -k YUM_NO_REVERT);
+    _YumCommand=$(/opt/elasticbeanstalk/bin/get-config environment -k YUM_COMMAND);
+
 
 function ReplaceFile (){
     echo "Source file is ${1}${3}"
@@ -29,9 +35,13 @@ function ReplaceFile (){
     cp -f ${1}${3} ${2}${3}
     echo "Staging ${4}${3}"
     echo "Target ${2}${3}"
+    };
 
     #Revert Back
-
+function RevertFile (){
+    echo "Begin Revert"
+    echo ${2}${3}
+    echo ${4}${3}
     if [ "$_YUM_DONT_REVERT" -eq "True" ] || [ "$_YUM_DONT_REVERT" -eq "1" ]
     then
     echo "Revert file to original: FALSE"
@@ -43,5 +53,14 @@ function ReplaceFile (){
     };
 
 
-   _YumConfFileBak=$(ReplaceFile $_YumConfFilePath  $_DefaultYumConfFilePath $_YumConfFile $_StagingYumConfFilePath);
-   echo $YumConfFileBak
+    ReplaceFile $_YumConfFilePath  $_DefaultYumConfFilePath $_YumConfFile $_StagingYumConfFilePath;
+    echo $YumConfFileBak
+    ReplaceFile $_YumRepoFilePath $_DefaultYumRepoFilePath $_YumRepoFile $_StagingYumRepoFilePath;
+
+    #Run Yum (Separate function of the above #Revert Back)
+    yum clean all;
+    echo $_YumCommand;
+    #Revert Back
+
+    RevertFile $_YumConfFilePath  $_DefaultYumConfFilePath $_YumConfFile $_StagingYumConfFilePath;
+    RevertFile $_YumRepoFilePath $_DefaultYumRepoFilePath $_YumRepoFile $_StagingYumRepoFilePath;
